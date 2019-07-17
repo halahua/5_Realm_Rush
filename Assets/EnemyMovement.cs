c06ae74f39c12e0c2f17cb3dc3988cb0a0201a99
João Traini <joao.traini@gmail.com>
2019-07-17 01:26:21 -0300

    5_RR_126

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    [SerializeField] List<Waypoint> path;
+
+
+    void Start()
+    {
+        StartCoroutine(FollowPath());
+        print("Back at Start.");
+    }
+
+    IEnumerator FollowPath()
+    {
+        print("Start patrol...");
+        foreach (Waypoint waypoint in path)
+        {
+            print("Visiting block: " + waypoint);
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(1f);
+        }
+        print("End patrol.");
+    }
+}
