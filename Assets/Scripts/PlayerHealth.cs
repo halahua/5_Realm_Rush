8edc27758ff0d126ef73b59cc0252c3ba0aeaae4
João Traini <joao.traini@gmail.com>
2019-07-29 23:40:35 -0300

    5_RR_157

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+
+    [SerializeField] int health = 10;
+    [SerializeField] int healthDecrease = 1;
+
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        health = health - healthDecrease;
+    }
+}
